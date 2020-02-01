0ee8f7d02e6e90d994ba08b5d335b5b231101832
Roskis <antti.karkinen@aalto.fi>
2020-02-01 15:23:06 +0200

    main nav

---
+++ Assets/Scripts/PathfindingMovement.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+public class PathfindingMovement : MonoBehaviour {
+    public Transform target;
+    private NavMeshPath path;
+    private float elapsed = 0.0f;
+    private NavMeshAgent agent;
+
+
+    private void Awake() {
+        path = new NavMeshPath();
+        elapsed = 0.0f;
+        agent = GetComponent<NavMeshAgent>();
+        //agent.destination = goal.position;
+    }
+
+    private void Update() {
+        // Update the way to the goal every second.
+        elapsed += Time.deltaTime;
+        if (elapsed > 1.0f)
+        {
+            elapsed -= 1.0f;
+            NavMesh.CalculatePath(transform.position, target.position, NavMesh.AllAreas, path);
+        }
+        if (path.status == UnityEngine.AI.NavMeshPathStatus.PathComplete) {
+            agent.path = path;
+        }
+        
+        for (int i = 0; i < path.corners.Length - 1; i++)
+            Debug.DrawLine(path.corners[i], path.corners[i + 1], Color.red);
+    }
+}
