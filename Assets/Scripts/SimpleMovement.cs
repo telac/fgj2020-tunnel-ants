fedd44c903aa85d583ae80b4a66297888a920112
Roskis <antti.karkinen@aalto.fi>
2020-02-01 12:49:26 +0200

    basic movement

---
+++ Assets/Scripts/SimpleMovement.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+public class SimpleMovement : MonoBehaviour {
+    public Transform goal;
+    private Vector3 oldPos;
+    private NavMeshAgent agent;
+    
+    private void Awake() {
+        agent = GetComponent<NavMeshAgent>();
+        agent.destination = goal.position;
+        oldPos = goal.position;
+    }
+
+    private void Update() {
+        if (goal.position != oldPos) {
+            agent.destination = goal.position;
+            oldPos = goal.position;
+        }
+    }
+}
