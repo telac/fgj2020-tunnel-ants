d64d0e6fcfffc4c7e142ef3dffae3417a6bb6da7
Christian Westerlund <ca.westerlund@gmail.com>
2020-02-02 12:28:26 +0200

    music

--- Assets/Scripts/BlockSpawner.cs
+++ Assets/Scripts/BlockSpawner.cs
@@ -23,11 +23,12 @@ public class BlockSpawner : MonoBehaviour
 
     IEnumerator StartSpawningRocks() {
         while(true) {
-            float _waitTime = Random.Range(0.0f, 5.0f);
+            float _waitTime = Random.Range(3.0f, 8.0f);
             yield return new WaitForSeconds(_waitTime);
             SpawnRock();
         }
     }
+
     void SpawnRock() {
         int i = Random.Range(0, rockSpawnPoints.Count);
         Instantiate(rockPrefab, rockSpawnPoints[i].position, rockPrefab.transform.rotation);
